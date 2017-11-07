54de3fd478ef040c10f88c4ee623d1adc33d2dfd
Oscar Bartra <obartra@taser.com>
2017-11-06 19:00:47 -0800

    feat(testing): Add unit and image comparison tests

--- params.js
+++ params.js
@@ -1,17 +1,19 @@
 // @preval
-const { dirname, resolve } = require('path')
+const { dirname, resolve, join } = require('path')
 const findUp = require('find-up')
 const rc = require('rc')
 const { name } = require('./package.json')
 
 const root = dirname(findUp.sync('package.json'))
+const picturebookPath = 'node_modules/picturebook/'
 const config = rc(name, {
   projectName: 'PictureBook',
-  markdownFooter: 'node_modules/picturebook/shared/storyFolders/footer.md',
+  entryPoint: join(picturebookPath, 'index.js'),
+  markdownFooter: join(picturebookPath, 'shared/storyFolders/footer.md'),
   projectUrl: 'https://github.com/obartra/picturebook',
   root,
 })
-;['markdownFooter', 'storyPath', 'entryPoint', 'postcssConfig'].forEach(key => {
+;['markdownFooter', 'storyPath', 'entryPoint', 'postcssConfig', 'jestConfig'].forEach(key => {
   if (key in config) {
     config[key] = resolve(config.root, config[key])
   }
