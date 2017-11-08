5a37a95c856bdfd8237b3c7c32d59f35b95f7cec
Oscar Bartra <obartra@taser.com>
2017-11-07 21:12:29 -0800

    fix(test): Correct nightwatch path

--- picturebook-image.sh
+++ picturebook-image.sh
@@ -4,4 +4,4 @@ set -e
 
 dir="$PWD/node_modules/picturebook"
 
-"$dir"/node_modules/.bin/nightwatch --config test/screenshot/nightwatch.conf.js $@
+"$dir"/node_modules/.bin/nightwatch --config "$dir"/test/screenshot/nightwatch.conf.js $@
